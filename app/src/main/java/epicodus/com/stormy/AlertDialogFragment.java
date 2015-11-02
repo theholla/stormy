9c26138f30c3e949807a5bb8835abd28451b934e
Diana Holland & Ashley Sullins <student@epicodus.com>
2015-11-02 11:24:35 -0800

    Add error handling and make successful request to Forecast API

---
+++ app/src/main/java/epicodus/com/stormy/AlertDialogFragment.java
@@ -0,0 +1,24 @@
+package epicodus.com.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+/**
+ * Created by Guest on 11/2/15.
+ */
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                        .setPositiveButton(context.getString(R.string.error_ok_message), null); //Tutorial didn't add null
+
+        AlertDialog dialog = builder.create();
+        return dialog;
+    }
+}
--- app/src/main/java/epicodus/com/stormy/MainActivity.java
+++ app/src/main/java/epicodus/com/stormy/MainActivity.java
@@ -1,17 +1,23 @@
 package epicodus.com.stormy;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
+import java.net.NetworkInterface;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,25 +29,56 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         String apiKey = "919b54fc3019d0acde79eef23e5c6838";
-        double latitude = 37.8267;
+        double latitude =  37.8267;
         double longitude = -122.423;
 
         String forecastUrl = "https://api.forecast.io/forecast/" + apiKey + "/" + latitude + "," + longitude;
 
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request
-                .Builder()
-                .url(forecastUrl)
-                .build();
-        Call call = client.newCall(request);
-        try {
-            Response response = call.execute();
-            if (response.isSuccessful()) {
-                Log.v(TAG, response.body().string());
-            }
-        } catch (IOException e) {
-            Log.e(TAG, "Exception caught: ", e);
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+
+                        if (response.isSuccessful()) {Log.v(TAG, response.body().string());
+                        }
+                        else {
+                            alertUserAboutError();
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, getString(R.string.exception_caught_message), e);
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
         }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
     }
 
 }
