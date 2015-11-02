7f18891b61779139985190bced286cb9f4468ab6
Diana Holland & Ashley Sullins <student@epicodus.com>
2015-11-02 13:45:18 -0800

    Add icons

---
+++ app/src/main/java/epicodus/com/stormy/CurrentWeather.java
@@ -0,0 +1,108 @@
+package epicodus.com.stormy;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by Guest on 11/2/15.
+ */
+public class CurrentWeather {
+    private String mIcon, mSummary, mTimeZone;
+    private long mTime;
+    private double mTemperature, mHumidity, mPrecipChance;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId() {
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        } else if (mIcon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        } else if (mIcon.equals("rain")) {
+            iconId = R.drawable.rain;
+        } else if (mIcon.equals("snow")) {
+            iconId = R.drawable.snow;
+        } else if (mIcon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        } else if (mIcon.equals("wind")) {
+            iconId = R.drawable.wind;
+        } else if (mIcon.equals("fog")) {
+            iconId = R.drawable.fog;
+        } else if (mIcon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        } else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        } else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public String getFormattedTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(getTimeZone()));
+        Date dateTime = new Date(getTime() *1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
+
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+}
